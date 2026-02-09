b158b2832106ec0ba10e72b5463ece05195a3403
Akriti Yadav <akritiyadav0214@gmail.com>
2026-02-09 15:26:02 +0600

    update

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
-console.log("hello world");
+function add(a, b) {
+    return a + b;
+}
+console.log(add(2, 3)); 
+const multiply =(a,b)=>a*b;
+let student={
+    name:"Akriti",
+    age:20,
+    course:"btech",
+}
+console.log(student.name);
+console.time("test");
+for(let i=0;i<10000;i++){}
+   console.timeEnd("test");
+   console.group("Student Details");
+   console.log("Name: Akriti Yadav");
+   console.log("Age: 20");
+   console.log("Course: BTech");
+   console.groupEnd();
+   console.count("a")
+   console.count("a")
+   let students={name:"akriti",age:20,course:"btech"};
+    console.table(students);
+   
